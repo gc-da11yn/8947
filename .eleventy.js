a4021ff739d7b2616977ee00305fae3883e2d786
Shawn Thompson <plansmash@me.com>
2021-12-30 09:11:59 -0500

    add project files

---
+++ .eleventy.js
@@ -0,0 +1,33 @@
+module.exports = function(eleventyConfig) {
+	const moment = require("moment");
+
+	// date filter (localized)
+	eleventyConfig.addNunjucksFilter("date", function (date, format, locale) {
+		locale = locale ? locale : "en";
+		moment.locale(locale);
+		return moment(date).format(format);
+	});
+
+	// Localized collections
+		eleventyConfig.addCollection("posts_en", function (collection) {
+		  return collection.getFilteredByGlob("./src/en/posts/*.md");
+		});
+	  
+		eleventyConfig.addCollection("posts_fr", function (collection) {
+		  return collection.getFilteredByGlob("./src/fr/posts/*.md");
+		});
+
+	return {
+			dir: {
+				input : "src",
+				output : "_site",
+				includes : "_includes",
+				data : "_data"
+			},
+			templateFormats : ["html", "md", "njk", "css"],
+			htmlTemplateEngine : "njk",
+			markdownTemplate : "njk",
+			setUseGitIgnore : false
+			// pathPrefix : "/gc-da11yn.github.io/"
+		};
+	}
